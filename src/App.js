f2c8caad535d03a403c09b21f1f9cd0640c7b8c3
Andrea Robles <cardavila13@gmail.com>
2022-02-14 20:44:06 +0100

    colocando estilos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import imagen1 from './img/img-1-mario.png';
+import imagen2 from './img/img-2-yoshi.png';
+import imagen3 from './img/img-3-luigi.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Proyecto 1 en React</h1>
+    <div className="contenedor">
+      <div className="caja">
+        <img src={imagen1}/>
+        <div className="texto">Mario Bros</div>
+      </div>
+
+      <div className="caja">
+        <img src={imagen2}/>
+        <div className="texto">Yoshi</div>
+      </div>
+
+      <div className="caja">
+        <img src={imagen3}/>
+        <div className="texto">Luigi</div>
+      </div>
     </div>
   );
 }
